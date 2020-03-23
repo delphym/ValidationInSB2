2b2e185aad45447d5da69cb312d219637783d46a
DelphyM <5643604+delphym@users.noreply.github.com>
2020-03-24 11:39:40 +1300

    Fixed the vlaidation test with a demonstration how to check for violatios

--- src/test/java/net/delphym/validation/UserTest.java
+++ src/test/java/net/delphym/validation/UserTest.java
@@ -1,6 +1,17 @@
 package net.delphym.validation;
 
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,16 +25,35 @@ import net.delphym.validation.model.User;
 @RunWith(SpringRunner.class)
 public class UserTest {
 
+	private static Validator validator;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		validator = Validation.buildDefaultValidatorFactory().getValidator();
+	}
+
 	@Test
-	public void createUser() {
+	public void createUserBlankNames() {
 		String name = "   ";
 		String email = " no email.here";
 
 		User newUser = new User(name, email);
 
+		Set<ConstraintViolation<User>> violations = validator.validate(newUser);
 		log.info("email: {}", newUser.getEmail());
 		log.info("id: {}", newUser.getId());
-		assertNotNull(newUser.getEmail());
-	}
+		log.info("user: {}", newUser);
+		log.info("violations: {}", violations);
 
+		assertThat(violations.size(), equalTo(2));
+
+		assertThat(violations.parallelStream().findFirst().get().getPropertyPath().toString(), equalTo("email"));
+		assertThat(violations.parallelStream().findAny().get().getMessage(), equalTo("Email is not valid"));
+
+		assertTrue(violations.parallelStream().filter(v->v.getPropertyPath().toString().equals("name")).findAny().isPresent());
+		assertTrue(violations.parallelStream().filter(v->v.getMessage().equals("Name is mandatory")).findAny().isPresent());
+
+		assertTrue(violations.parallelStream().filter(v->v.getPropertyPath().toString().equals("email")).findAny().isPresent());
+		assertTrue(violations.parallelStream().filter(v->v.getMessage().equals("Email is not valid")).findAny().isPresent());
+	}
 }
